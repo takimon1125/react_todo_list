521b5877514df72dc90f7a836ba8b24021500235
takimon1125 <>
2025-10-06 00:49:09 +0900

    useMemoでタスクの数表示をmemo化

--- src/components/TodoSummary.jsx
+++ src/components/TodoSummary.jsx
@@ -1,26 +1,20 @@
+import { useMemo } from "react";
+
 const TodoSummary = ({ todos }) => {
+  const { total, completed, pending } = useMemo(
+    () => ({
+      total: todos.length,
+      completed: todos.filter((todo) => todo.isCompleted).length,
+      pending: todos.filter((todo) => !todo.isCompleted).length,
+    }),
+    [todos]
+  );
+
   return (
-    <div className="flex m-3 ">
-      <p className="mr-3">
-        全てのタスク：
-        {todos.length}
-      </p>
-      <p className="mr-3">
-        完了済み：
-        {
-          todos.filter((todo) => {
-            return todo.isCompleted;
-          }).length
-        }
-      </p>
-      <p className="mr-3">
-        未完了：
-        {
-          todos.filter((todo) => {
-            return !todo.isCompleted;
-          }).length
-        }
-      </p>
+    <div className="flex m-3">
+      <p className="mr-3">全てのタスク：{total}</p>
+      <p className="mr-3">完了済み：{completed}</p>
+      <p className="mr-3">未完了：{pending}</p>
     </div>
   );
 };
